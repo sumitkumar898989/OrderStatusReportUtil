115d6c1781a1d563d262e2bfe690b09ad2c89628
sumitkumar898989 <113261917+sumitkumar898989@users.noreply.github.com>
2022-09-16 14:11:56 +0530

    removed the credentials of twilio and set it to environment variable.

--- OrderStatusReportUtil/src/main/java/com/capgemini/service/SMSService.java
+++ OrderStatusReportUtil/src/main/java/com/capgemini/service/SMSService.java
@@ -13,7 +13,7 @@ public class SMSService{
   public  void sendSMS(){
 	  
 	System.out.println("in SMSService sendSMS method");
-	Twilio.init("AC5ef8252f9f7ae2dba8c8b184ed3a4d3f","1da592e9aa0d6a74831bcc72f4748f29");
+	Twilio.init(System.getenv("TWILIO_ACCOUNT_SID"), System.getenv("TWILIO_AUTH_TOKEN"));
 
     Message.creator(new PhoneNumber("+917410772589"),
                     new PhoneNumber("+18147475433"), "Order failure reached at crtical point request your attention").create();
